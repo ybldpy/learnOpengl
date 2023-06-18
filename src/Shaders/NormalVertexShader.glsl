da8105f60d9ec544895ec462dae07a96169e46ba
ybldnpy <18508782239@163.com>
2023-06-18 20:05:58 +0800

    texture lighting

--- src/Shaders/NormalVertexShader.glsl
+++
@@ -1,8 +0,0 @@
-#version 330 core
-
-layout(location = 0) in vec3 aPos;
-
-uniform mat4 trans;
-void main() {
-    gl_Position = trans*vec4(aPos,1.0);
-}
--- src/stbi_img_impl.cpp
+++
@@ -1,2 +0,0 @@
-﻿#define STB_IMAGE_IMPLEMENTATION
-#include "std_image.h"
